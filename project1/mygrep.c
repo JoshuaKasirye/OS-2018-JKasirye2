c21936ec2614b96b9c7a7aa929027a0d690a23e6
JoshuaKasirye <30349891+JoshuaKasirye@users.noreply.github.com>
2018-09-20 10:54:38 +0100

    Update mygrep.c

--- project1/mygrep.c
+++ project1/mygrep.c
@@ -1,35 +1,33 @@
-
 #include <stdio.h> //std library
 #include <stdlib.h> // For exit()
 #include <string.h> // string lib
 #define  MAXStr_LEN  100 // Maximum
+int i;
 
-int main(char *argc, char *argv[])
+int main(int argc, char *argv[])
 {
     FILE *datafile;
     char line[MAXStr_LEN];
     char *dataline; //all characters per line
 
-    datafile = fopen(argv[2], "r");
-    if (datafile == NULL)
-    {
-        printf("my-grep: cannot open file %s \n",argv[2]);
-        exit(1);}
-
-    dataline = fgets(line,MAXStr_LEN,datafile);
-    if (datafile != NULL )
-    {
-        if (argv[1] == " "){
-            exit(0);}
-        else{  //if argv[0]it's not empty string
-            while( dataline!= NULL){
-                if(strstr(dataline,argv[1])!=0){
-                    printf("%s",dataline);}
-                dataline = fgets(line,MAXStr_LEN,datafile);}
+    for(i=2;i<argc;i++){
+        datafile = fopen(argv[i], "r");
+        if(datafile == NULL){
+            printf("mygrep: cannot open file %s \n",argv[i]);
+            exit(1);}
+        dataline = fgets(line,MAXStr_LEN,datafile);
+        if (datafile != NULL ){
+            if (argv[1] == " "){
+                exit(0);}
+            else{  //if argv[0]it's not empty string
+                while( dataline!= NULL){
+                    if(strstr(dataline,argv[1])!=0){
+                        printf("%s",dataline);}
+                    dataline = fgets(line,MAXStr_LEN,datafile);}
+                }
+            }
+        fclose(datafile);
         }
-    }
-    fclose(datafile);
     return 0;
 }
-
 
