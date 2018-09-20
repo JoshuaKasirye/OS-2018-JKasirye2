f2d1107768652398da56ad94d1013f5e1075b48c
JoshuaKasirye <30349891+JoshuaKasirye@users.noreply.github.com>
2018-09-20 11:08:57 +0100

    Update mygrep.c

--- project1/mygrep.c
+++ project1/mygrep.c
@@ -1,21 +1,25 @@
-#include <stdio.h> //std library
-#include <stdlib.h> // For exit()
+//Created by Joshua Kasirye
+//OS project 1
+//57442019
+
+#include <stdio.h> //std C library
+#include <stdlib.h> // To enable use of exit()
 #include <string.h> // string lib
-#define  MAXStr_LEN  100 // Maximum
-int i;
+#define  MAXStr_LEN  100 // Maximum str length
+int i; //for loop varibale
 
 int main(int argc, char *argv[])
 {
-    FILE *datafile;
+    FILE *datafile;  //file varibale name
     char line[MAXStr_LEN];
     char *dataline; //all characters per line
 
     for(i=2;i<argc;i++){
-        datafile = fopen(argv[i], "r");
+        datafile = fopen(argv[i], "r"); //opening file for only reading
         if(datafile == NULL){
             printf("mygrep: cannot open file %s \n",argv[i]);
             exit(1);}
-        dataline = fgets(line,MAXStr_LEN,datafile);
+        dataline = fgets(line,MAXStr_LEN,datafile); //getting the string of characters in a line
         if (datafile != NULL ){
             if (argv[1] == " "){
                 exit(0);}
@@ -23,10 +27,11 @@ int main(int argc, char *argv[])
                 while( dataline!= NULL){
                     if(strstr(dataline,argv[1])!=0){
                         printf("%s",dataline);}
-                    dataline = fgets(line,MAXStr_LEN,datafile);}
+                    dataline = fgets(line,MAXStr_LEN,datafile);} //updating the dataline variable
                 }
             }
-        fclose(datafile);
+        fclose(datafile); // closing every file after being read from
+        printf(" ");
         }
     return 0;
 }
